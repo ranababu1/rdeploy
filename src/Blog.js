cae2616ccdbb3407ab97f7c9a68cdc93d0bedfe4
Imran <imranul.mazumder@iquanti.com>
2020-11-17 21:43:15 +0530

    blog page added

--- src/Blog.js
+++ src/Blog.js
@@ -1,12 +1,98 @@
 import React from 'react';
- 
-const Blog = ()=>{
-   return (
-       <div className="container App">
-       <h1>Blog Index</h1>
-       <h3>coming soon...</h3>
-   </div>
-   )
+
+const Blog = () => {
+return (
+<div className="contentPart">
+    <div className="container">
+        <div className="row">
+            <div className="col s12 m8">
+                <h3 className="header">Recent Posts</h3>
+                <div className="card hoverable">
+                    <div className="card-image">
+                        <img
+                            src="https://thumbs.dreamstime.com/b/banner-background-colorful-holi-powder-snow-multiple-colors-surface-146214908.jpg" />
+                    </div>
+                    <div className="card-stacked colorGrey">
+                        <div className="card-content">
+                            <h4>Title of the post</h4>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="card hoverable">
+                    <div className="card-image">
+                        <img
+                            src="https://thumbs.dreamstime.com/b/banner-background-colorful-holi-powder-snow-multiple-colors-surface-146214908.jpg" />
+                    </div>
+                    <div className="card-stacked colorGrey">
+                        <div className="card-content">
+                            <h4>Gaming PC under Rs 50000
+                            </h4>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="card hoverable">
+                    <div className="card-image">
+                        <img
+                            src="https://thumbs.dreamstime.com/b/banner-background-colorful-holi-powder-snow-multiple-colors-surface-146214908.jpg" />
+                    </div>
+                    <div className="card-stacked colorGrey">
+                        <div className="card-content">
+                            <h4>Best Zoom Alternative For Online Meetings </h4>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="card hoverable">
+                    <div className="card-image">
+                        <img
+                            src="https://thumbs.dreamstime.com/b/banner-background-colorful-holi-powder-snow-multiple-colors-surface-146214908.jpg" />
+                    </div>
+                    <div className="card-stacked colorGrey">
+                        <div className="card-content">
+                            <h4>New Features of Android 10
+                            </h4>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="card hoverable">
+                    <div className="card-image">
+                        <img
+                            src="https://thumbs.dreamstime.com/b/banner-background-colorful-holi-powder-snow-multiple-colors-surface-146214908.jpg" />
+                    </div>
+                    <div className="card-stacked colorGrey">
+                        <div className="card-content">
+                            <h4>Best Payment Wallet In India
+                            </h4>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div className="col s12 m4">
+                <h3 className="header">Topics</h3>
+                <div className="sidebar">
+                    <p>Tech Updates</p>
+                    <p>TIL</p>
+                    <p>Gadget Updates</p>
+                    <p>DIY</p>
+                </div>
+                <h4 className="header">Newsletter</h4>
+                <div className="sidebar">
+                    <p>Enter you email to get free updates</p>
+                    <p>Email Input Box</p>
+                </div>
+                <h4 className="header">Ad Space</h4>
+                <div className="sidebar">
+                    <p>Adsense Box goes here</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+)
 }
- 
+
 export default Blog;
